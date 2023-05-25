55aae97eb4d74f25c921dad3da75892debd6bddc
puppylpg <shininglhb@163.com>
2023-05-25 18:22:04 +0800

    flask! ui

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
 
-SEARCH_SIZE = 10
+SEARCH_SIZE = 100
 INDEX_NAME = os.environ['INDEX_NAME']
 app = Flask(__name__)
 
@@ -24,26 +24,23 @@ def analyzer():
     query = request.args.get('q')
     query_vector = model.encode([query])[0].tolist()
 
-    script_query = {
-        "script_score": {
-            "query": {"match_all": {}},
-            "script": {
-                "source": "cosineSimilarity(params.query_vector, doc['text_vector']) + 1.0",
-                "params": {"query_vector": query_vector}
-            }
-        }
+    body = {
+        "size": SEARCH_SIZE,
+        "knn": {
+            "field": "text_vector",
+            "query_vector": query_vector,
+            "k": SEARCH_SIZE,
+            "num_candidates": SEARCH_SIZE + 100
+        },
+        "_source": {"includes": ["all", "title"]}
     }
 
     response = client.search(
         index=INDEX_NAME,
-        body={
-            "size": SEARCH_SIZE,
-            "query": script_query,
-            "_source": {"includes": ["title", "description", "tags"]}
-        }
+        body=body
     )
     print(query)
-    pprint(response)
+    # pprint(response)
     return jsonify(response)
 
 
